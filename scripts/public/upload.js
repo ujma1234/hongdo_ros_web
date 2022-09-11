93e027f1b05920c3bc0ebc5fd55d0f4ae9ba417e
ujma1234 <ujma1234@naver.com>
2022-09-11 21:59:27 +0900

    upload modify

--- scripts/public/upload.js
+++ scripts/public/upload.js
@@ -1,93 +1,52 @@
-// const timeout1 = setTimeout(()=> {
-//     document.getElementById("hong2_box").className = "hong2";
-// }, 2500)
 
-// var voice = new Audio('audio/speech/2.mp3');
-// voice.muted = true;
-// voice.play();
 
-// setTimeout(()=>{
-//   voice.muted = false;
-// },4000);
 
-// const timeout3 = setTimeout(()=> {
-    
-//     document.getElementById("camera_box").className = "btn";
 
-//     document.getElementById("txt1").className = "text"
-// }, 4500)
+var myVideoStream = document.getElementById('myVideo')     // make it a global variable
+  var myStoredInterval = 0
 
-// const btn = document.getElementById("camera_box");
-// btn.addEventListener("click", e=>{
-//     btn.className = "off_btn";
-//     document.getElementById("display").className = "display";
-//     document.getElementById("txt1").className = "off_text";
-//     document.getElementById("txt").className = "text";
-    
+function getVideo(){
+  navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-//     const timeout6 = setTimeout(()=> {
-//         document.getElementById("txt2").className = "anounce";
-//     }, 2000);
-
-//     const timeout4 = setTimeout(()=> {
-//         document.getElementById("count").className = "demo";
-//     }, 4000);
-
-//     setTimeout(()=>{
-//       myVideoStream.remove();
-//     },6900);
-
-//     const timeout5 = setTimeout(()=>{
-//         location.replace("taking_pic.html")
-        
-//     }, 7000);
-// })
-
-// var myVideoStream = document.getElementById('myVideo')     // make it a global variable
-//   var myStoredInterval = 0
-
-// function getVideo(){
-//   navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-//   navigator.getMedia({
-//     video: {
-//         deviceId:"ec24fa6365362930858ecd21b8f258fce23c1ab63fe6e39a8a8082ffeda769e0"
-//     }, 
-//     audio: false
-// },
+  navigator.getMedia({
+    video: {
+        deviceId:"ec24fa6365362930858ecd21b8f258fce23c1ab63fe6e39a8a8082ffeda769e0"
+    }, 
+    audio: false
+},
                      
-//     function(stream) {
-//       myVideoStream.srcObject = stream   
-//       myVideoStream.play();
-//   }, 
+    function(stream) {
+      myVideoStream.srcObject = stream   
+      myVideoStream.play();
+  }, 
                      
-//    function(error) {
-//      alert('webcam not working');
-//   });
-// }
+   function(error) {
+     alert('webcam not working');
+  });
+}
 
-// if (!navigator.mediaDevices?.enumerateDevices) {
-//     console.log("enumerateDevices() not supported.");
-//   } else {
-//     // List cameras and microphones.
-//     navigator.mediaDevices.enumerateDevices()
-//       .then((devices) => {
-//         devices.forEach((device) => {
-//           console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
-//         });
-//       })
-//       .catch((err) => {
-//         console.error(`${err.name}: ${err.message}`);
-//       });
-//   }
+if (!navigator.mediaDevices?.enumerateDevices) {
+    console.log("enumerateDevices() not supported.");
+  } else {
+    // List cameras and microphones.
+    navigator.mediaDevices.enumerateDevices()
+      .then((devices) => {
+        devices.forEach((device) => {
+          console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
+        });
+      })
+      .catch((err) => {
+        console.error(`${err.name}: ${err.message}`);
+      });
+  }
 
-//   const btn_home = document.getElementById("btn_home");
+  const btn_home = document.getElementById("btn_home");
 
-// var go_home = 0;
+var go_home = 0;
 
-// btn_home.addEventListener("click",e=>{
-//     go_home++;
-//     if(go_home >= 5) {
-//         location.replace("intro.html");
-//     }
-// });
+btn_home.addEventListener("click",e=>{
+    go_home++;
+    if(go_home >= 5) {
+        location.replace("intro.html");
+    }
+});
