40ade5a939f9e5b3e0d5bdcb9cf3ed026d4a25b2
ujma1234 <ujma1234@naver.com>
2022-09-12 18:05:33 +0900

    Modify drawn page

--- scripts/public/drawn.js
+++ scripts/public/drawn.js
@@ -1,40 +1,6 @@
 var duru_audio = new Audio('audio/effect/duru.mp3');
 duru_audio.muted= true;
 
-
-
-
-const timeout = setTimeout(()=>{
-    document.getElementById("real_bg").className = "real_bg";
-},100);
-const timeout1 = setTimeout(()=>{
-    document.getElementById("real_bg").className = "off_real_bg";
-    document.getElementById("bg").className = "off_real_bg";
-},1100);
-const timeout3 = setTimeout(()=>{
-    duru_audio.play();
-    duru_audio.muted = false;
-    document.getElementById("box1").className = "img_box";
-    document.getElementById("box2").className = "QR_box";
-    document.getElementById("txt1").className = "announce";
-    document.getElementById("txt2").className = "text";
-},1200);
-
-
-const next  = document.getElementById("txt2");
-next.addEventListener("click", e=>{
-    document.getElementById("txt2").className = "bye_txt";
-
-    const timeout3 = setTimeout(()=>{
-        document.getElementById("bg").className = "bye_bg";
-    },1000);
-    const timeout4 = setTimeout(()=>{
-        location.replace("game_intro.html");
-    },3000);
-});
-
-
-
 const btn_home = document.getElementById("btn_home");
 
 var go_home = 0;
@@ -46,3 +12,8 @@ btn_home.addEventListener("click",e=>{
     }
 });
 
+const btn_next = document.getElementById("next_btn");
+
+btn_next.addEventListener("click", e=>{
+    location.replace("intro.html");
+});
