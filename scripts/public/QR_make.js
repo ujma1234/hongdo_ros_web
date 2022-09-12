7a88765c9cc4e46d514668dba1c480d095ea5228
ujma1234 <ujma1234@naver.com>
2022-09-12 16:40:07 +0900

    Add QR_make

--- scripts/public/QR_make.js
+++ scripts/public/QR_make.js
@@ -1,26 +1,3 @@
-const button = document.querySelector('.button');
-const submit = document.querySelector('.submit');
-
-function toggleClass() {
-  this.classList.toggle('active');
-}
-
-function addClass() {
-  this.classList.add('finished');
-}
-
-button.addEventListener('click', toggleClass);
-button.addEventListener('click', e=>{
-    const timeout = setTimeout(()=>{
-        location.replace("drawn.html");
-    },5000);
-});
-
-button.addEventListener('transitionend', toggleClass);
-button.addEventListener('transitionend', addClass);
-
-
-
 
 const btn_home = document.getElementById("btn_home");
 
