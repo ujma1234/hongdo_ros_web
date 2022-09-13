9e8e3728a6100f8686f03f55d98fce78d6dc8c18
ujma1234 <ujma1234@naver.com>
2022-09-13 19:02:14 +0900

    Modify game_intro page

--- scripts/public/game_intro.js
+++ scripts/public/game_intro.js
@@ -4,46 +4,18 @@ var voice5_audio = new Audio('audio/speech/5.mp3');
 voice5_audio.muted = true;
 const no = document.getElementById("no");
 
-voice5_audio.play();
-voice5_audio.muted=false;
+setTimeout(()=>{
+    voice5_audio.play();
+    voice5_audio.muted=false;
+},2500);
+
+setTimeout(()=>{
+
+},17500);
 
 
-no.addEventListener("click", e=>{
-    document.getElementById("wrn").className =
-    "warning";
-});
 
-const wno = document.getElementById("wno");
 
-wno.addEventListener("click", e=>{
-    document.getElementById("wrn").className = "offing_warning";
-
-    const timeout = setTimeout(()=>{
-        document.getElementById("wrn").className = "off_warning";
-    },1500);
-});
-
-const yes = document.getElementById("yes");
-
-yes.addEventListener("click", e=>{
-    document.querySelector(".off_container").className = "container";
-
-    setTimeout(()=>{
-        location.replace("game.html");
-    },2000);
-    
-});
-
-const wyes = document.getElementById("wyes");
-
-wyes.addEventListener("click", e=>{
-    document.querySelector(".off_container").className = "container";
-
-    setTimeout(()=>{
-        location.replace("finish.html");
-    },2000);
-
-});
 
 const btn_home = document.getElementById("btn_home");
 
