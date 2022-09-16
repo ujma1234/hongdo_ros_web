9b76452d1045cc630962f5e4e652478e55d5b01f
ujma1234 <ujma1234@naver.com>
2022-09-16 23:20:01 +0900

    Hot fix

--- scripts/talker.js
+++ scripts/talker.js
@@ -57,6 +57,9 @@ function handshake_service() {
 function url_service(urlstring) {
   const client = nh.serviceClient('/make_qr', 'hongdo_ros_webconnect/UrlTunnel');
   client.call({url : urlstring})
+  .then((res) =>{
+    return res;
+  });
 }
 
 
@@ -143,15 +146,18 @@ if (require.main === module) {
   })
 
   app.get('/QR_make.html', (req,res) => {
-
-    imgbbUploader("e4422a3845100fe670775736ffd0e7cb", '/home/jeonghan/catkin_ws/src/hongdo_ros/hongdo_ros_web/scripts/public/hongdo_AI/output/trained_model.png'). then((response)=>
-      url_service(JSON.stringify(response.url))
+    res.sendFile(__dirname+'/QR_make.html');
+    imgbbUploader("e4422a3845100fe670775736ffd0e7cb", '/home/jeonghan/catkin_ws/src/hongdo_ros/hongdo_ros_web/scripts/public/hongdo_AI/output/trained_model.png'). then((response)=> {
+      if( url_service(JSON.stringify(response.url)) == true ) {
+        return res.redirect("/drawn.html");
+      }
+    }
       // console.log(JSON.stringify(response.url))
     )
     .catch((error) => 
       console.error(error)
     );
-    res.sendFile(__dirname+'/QR_make.html');
+    
   })
 
 
